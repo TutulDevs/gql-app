6c4b77f43a4b492415aa7c91289b859804e7f1a8
Tutul <tutulnahid@gmail.com>
2021-08-31 21:37:17 +0600

    added env in the client side

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,7 @@ import reportWebVitals from "./reportWebVitals";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql",
+  uri: process.env.REACT_APP_API_KEY,
   cache: new InMemoryCache(),
   connectToDevTools: true,
 });
